a8d596efb1750e7b663d146d6d84e6809b238f94
kenforever <ken200203231@gmail.com>
2021-01-05 03:37:42 +0800

    Update exchange_rate.py

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,6 +1,6 @@
 import requests
 base_url = "https://rest.coinapi.io/v1/exchangerate/"
-headers = {'X-CoinAPI-Key' : '4DD30C3D-65FF-43B4-9309-51B6BE76FE16'}
+headers = {'X-CoinAPI-Key' : 'YOURKEYHERE'}
 
 def exchange_rate(TYPE,FROM): #TYPE為對象, FROM為被換者
     url = base_url+FROM+"/"+TYPE
